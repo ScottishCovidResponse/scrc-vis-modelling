766c48e74d6c9474e79fae8bb01f78197824942f
maxie12 <max.sondag@hotmail.com>
2021-07-21 10:38:10 +0100

    Removing commented code

--- ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/experiments/utils/greedyKcover.py
+++ ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/experiments/utils/greedyKcover.py
@@ -15,11 +15,8 @@ def greedyBS(SP, N, K = 10, la = 0.0, ua = 0.0):
                     maxCost = info[0]
 
     weights = {src: sorted([(info[0]/norm, sink, info[-1]) for sink, info in sinks.items()]) for src, sinks in SP.items()}
-    #print weights.values()
 
-    #ua = K*N*maxCost/norm
     ua = K*N*maxCost/norm
-    #ua = N*maxCost/norm
 
     open_cost = {s: 1.0 for s in weights}
 
@@ -28,9 +25,7 @@ def greedyBS(SP, N, K = 10, la = 0.0, ua = 0.0):
     alpha = (la + ua)/2.0
     legal_cover = {}
     legal_alpha = -1
-    #alpha = ua
-    #alpha = 1.0
-    #for iter in xrange(100):
+
     iter = 0
     while iter < 100 or (ua - la) > 1e-8:
         iter += 1
@@ -39,10 +34,10 @@ def greedyBS(SP, N, K = 10, la = 0.0, ua = 0.0):
         cover = {}
         total_cost = 0.0
         while len(covered_nodes) < N:
-        #for k in xrange(K, 0, -1):
+
             best_src, best_mgain, best_cost, best_covered_nodes = -1, np.Inf, np.Inf, set()
             for src, sinks in weights.items():
-                #if src not in cover: #with or without repetition
+
                     m_profit = 0.0
                     cost = open_cost[src] * alpha
                     local_best_mgain, local_best_cost, local_best_covered_nodes = np.Inf, np.Inf, set()
@@ -63,7 +58,7 @@ def greedyBS(SP, N, K = 10, la = 0.0, ua = 0.0):
                         best_covered_nodes = copy.deepcopy(local_best_covered_nodes)
 
             covered_nodes.update(best_covered_nodes)
-            #N -= len(best_covered_nodes)
+
             cover[best_src] = cover.get(best_src, []) + list(best_covered_nodes)
             #print('best cost', best_cost)
             if best_cost != np.Inf:
@@ -86,11 +81,11 @@ def greedyBS(SP, N, K = 10, la = 0.0, ua = 0.0):
 
                 break
         alpha = (ua + la)/2.0
-      #  print('alpha', la, ua, alpha)
 
     if not legal_cover:
         legal_cover = cover
         legal_alpha = alpha
+        
     print('num of srcs', len(legal_cover))
     for src, sinks in legal_cover.items():
         for s in sinks:

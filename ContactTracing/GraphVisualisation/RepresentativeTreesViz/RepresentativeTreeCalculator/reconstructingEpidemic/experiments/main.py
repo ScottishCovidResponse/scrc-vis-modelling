a556c48055638175915232de2a06e3103382b31d
maxie12 <max.sondag@hotmail.com>
2021-08-02 10:41:01 +0100

    Parser working

--- ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/experiments/main.py
+++ ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/experiments/main.py
@@ -2,20 +2,20 @@ from compute_paths import *
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("-n","--nodes", help="Location of the inputfile for nodes. Format is (nid \t unixtimestamp). timestamp is time of report. Can be empty")
-parser.add_argument("-e","--edges", help="Location of the inputfile for edges. Format is (unixtimestamp \t n1id \t n2id \t weight). timestamp is time of interaction")
-parser.add_argument("-o","--output", help="Location of the outputfile")
+parser.add_argument("-n","--nodes",required=True, help="Location of the inputfile for nodes. Format is (nid \t unixtimestamp). timestamp is time of report. Can be empty")
+parser.add_argument("-e","--edges",required=True, help="Location of the inputfile for edges. Format is (unixtimestamp \t n1id \t n2id \t weight). timestamp is time of interaction")
+parser.add_argument("-o","--output",required=True, help="Location of the outputfile")
 
 args = parser.parse_args()
 
-#filepath_nodes = parser.nodes
-#filepath_edges = parser.edges
-#filepath_output = parser.output
+filepath_nodes = args.nodes
+filepath_edges = args.edges
+filepath_output = args.output
 
 
-filepath_edges = "F:/Development/Swansea/scrc-vis-modelling/ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/Data/DataEdge.txt";
-filepath_nodes = "F:/Development/Swansea/scrc-vis-modelling/ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/Data/DataNode.txt";
-filepath_output = "F:/Development/Swansea/scrc-vis-modelling/ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/Data/output.txt";
+#filepath_edges = "F:/Development/Swansea/scrc-vis-modelling/ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/Data/DataEdge.txt";
+#filepath_nodes = "F:/Development/Swansea/scrc-vis-modelling/ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/Data/DataNode.txt";
+#filepath_output = "F:/Development/Swansea/scrc-vis-modelling/ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/reconstructingEpidemic/Data/output.txt";
 
 
 K=1

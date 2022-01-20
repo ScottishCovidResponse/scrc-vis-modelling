02d6e969313bfec99bca6002c0e09c8108725c69
maxie12 <max.sondag@hotmail.com>
2022-01-20 13:11:29 +0000

    Added tree constructor with id

--- ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/src/InfectionTreeGenerator/Graph/Tree.java
+++ ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/src/InfectionTreeGenerator/Graph/Tree.java
@@ -23,6 +23,15 @@ import java.util.Set;
 //public class Tree<N extends Node<E>, E extends Edge<N>> extends Graph<N extends Node<E>, E extends Edge<N>> {
 public class Tree<N extends Node<E>, E extends Edge<N>> extends Graph {
 
+    public Tree() {
+        super();
+    }
+
+    public Tree(int id) {
+        super();
+        this.id = id;
+    }
+
     //returns if possibleAncestor is an ancestor of n
     public boolean isAncestor(N possibleAncestor, N n) {
 
@@ -190,7 +199,4 @@ public class Tree<N extends Node<E>, E extends Edge<N>> extends Graph {
         return children;
     }
 
-    
-    
-    
 }

5a4241300b0f9765ebf13909a57467fefe581d20
maxie12 <max.sondag@hotmail.com>
2022-01-20 13:19:08 +0000

    Added a less memory intensive variant of calculating dominating sets not requiring the graph

---
+++ ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/src/InfectionTreeGenerator/Graph/GraphAlgorithms/DominatingSetCalculatorImplicit.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InfectionTreeGenerator.Graph.GraphAlgorithms;
+
+import InfectionTreeGenerator.Graph.Edge;
+import InfectionTreeGenerator.Graph.Graph;
+import InfectionTreeGenerator.Graph.GraphAlgorithms.DistanceMeasures.TreeDistanceMeasure;
+import InfectionTreeGenerator.Graph.Node;
+import InfectionTreeGenerator.Graph.Tree;
+import Utility.Log;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Uses an implicit graph representation instead of an explicit one to save
+ * meory
+ *
+ * @author MaxSondag
+ */
+public class DominatingSetCalculatorImplicit {
+
+    public List<Integer> getDominatingSet(int maxDistance, List<Tree> trees, TreeDistanceMeasure dm) {
+        ArrayList<Integer> dominatingSet = new ArrayList();
+        //nodes that are not connected always need to be in
+
+        //Trivial algorithm. Go through the nodes. For each node if it is not yet dominated, add it to the set.
+        for (Tree t : trees) {
+            if (!isDominated(dominatingSet, t, trees, dm, maxDistance)) {
+                dominatingSet.add(t.id);
+            }
+        }
+        return dominatingSet;
+    }
+
+    /**
+     * Returns a new dominating set containing only the ids of the nodes from
+     * {@code currentDsIds} to dominate trees
+     *
+     * @param currentDomSet
+     * @param trees
+     * @param dm
+     * @param maxDistance
+     * @return 
+     */
+    public List<Integer> trimDominatingSet(List<Integer> currentDomSet, List<Tree> trees, TreeDistanceMeasure dm, int maxDistance) {
+        //start by taking all trees, and keep removing them as long as the result remaind a dominating set
+        ArrayList<Integer> trimmedDomSet = new ArrayList(currentDomSet);
+        ArrayList<Integer> idsToConsider = new ArrayList(currentDomSet);
+
+        for (Integer id : idsToConsider) {
+            //check if removing node with id from the dominating set still gives a dominating set
+            trimmedDomSet.remove(id);
+            if (!isDominatingSet(trimmedDomSet, trees, dm, maxDistance)) {
+                //not a dominating set anymore, put it back
+                trimmedDomSet.add(id);
+            }
+        }
+
+        return trimmedDomSet;
+    }
+
+    private boolean isDominatingSet(ArrayList<Integer> candidateDomSet, List<Tree> trees, TreeDistanceMeasure dm, int maxDistance) {
+        for (Tree t : trees) {
+            if (!isDominated(candidateDomSet, t, trees, dm, maxDistance)) {
+                return false;//t is not dominated
+            }
+        }
+        return true;//all nodes are dominated
+    }
+
+    private boolean isDominated(ArrayList<Integer> domSet, Tree t, List<Tree> trees, TreeDistanceMeasure dm, double maxDistance) {
+        if (domSet.contains(t.id)) {
+            return true;
+        }
+
+        //go through all trees in the dominating set, and check if one of them dominates this tree
+        for (Integer id : domSet) {
+            Tree tId = getTreeById(trees, id);
+            double distance = dm.getDistance(tId, t);
+            if (distance <= maxDistance) {
+                return true; //t is dominated by tId
+            }
+        }
+        //no tree dominates this node
+        return false;
+    }
+
+    private Tree getTreeById(List<Tree> trees, Integer id) {
+        for (Tree t : trees) {
+            if (t.id == id) {
+                return t;
+            }
+        }
+        System.err.println("No tree with id:" + id + "exists");
+        return null;
+    }
+
+}
---
+++ ContactTracing/GraphVisualisation/RepresentativeTreesViz/RepresentativeTreeCalculator/test/InfectionTreeGenerator/Graph/GraphAlgorithms/DominatingSetCalculatorImplicitTest.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InfectionTreeGenerator.Graph.GraphAlgorithms;
+
+import InfectionTreeGenerator.Graph.Edge;
+import InfectionTreeGenerator.Graph.Graph;
+import InfectionTreeGenerator.Graph.GraphAlgorithms.DistanceMeasures.TreeDistanceMeasure;
+import InfectionTreeGenerator.Graph.Infection.InfectionEdge;
+import InfectionTreeGenerator.Graph.Infection.InfectionNode;
+import InfectionTreeGenerator.Graph.Node;
+import InfectionTreeGenerator.Graph.Tree;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MaxSondag
+ */
+public class DominatingSetCalculatorImplicitTest {
+
+    Tree t1N = new Tree(1);
+    Tree t2N = new Tree(2);
+    Tree t3N = new Tree(3);
+    Tree t4N = new Tree(4);
+    Tree t5N = new Tree(5);
+    Tree t6N = new Tree(6);
+    Tree t7N = new Tree(7);
+
+    List<Tree> trees = Arrays.asList(t1N, t2N, t3N, t4N, t5N, t6N, t7N);
+
+    DominatingSetCalculatorImplicit instance;
+
+    private class MockTreeDistanceMeasure implements TreeDistanceMeasure {
+
+        @Override
+        public double getDistance(Tree<InfectionNode, InfectionEdge> t1, Tree<InfectionNode, InfectionEdge> t2) {
+            return Math.max(t1.id, t2.id) - Math.min(t1.id, t2.id);
+        }
+
+    }
+
+    TreeDistanceMeasure dm = new MockTreeDistanceMeasure();
+
+    public DominatingSetCalculatorImplicitTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getDominatingSet method, of class
+     * DominatingSetCalculatorImplicit.
+     */
+    @Test
+    public void testGetDominatingSet() {
+        DominatingSetCalculatorImplicit dsc = new DominatingSetCalculatorImplicit();
+        List<Integer> dominatingSet = dsc.getDominatingSet(0, trees, dm);
+        assertEquals(7, dominatingSet.size()); //all nodes
+        assertTrue(dominatingSet.contains(1));
+        assertTrue(dominatingSet.contains(2));
+        assertTrue(dominatingSet.contains(3));
+        assertTrue(dominatingSet.contains(4));
+        assertTrue(dominatingSet.contains(5));
+        assertTrue(dominatingSet.contains(6));
+        assertTrue(dominatingSet.contains(7));
+
+        //sensible implementation should have at most this many nodes
+        dominatingSet = dsc.getDominatingSet(1, trees, dm);
+        assertTrue(dominatingSet.size() <= 4);
+
+        dominatingSet = dsc.getDominatingSet(2, trees, dm);
+        assertTrue(dominatingSet.size() <= 3);
+
+        dominatingSet = dsc.getDominatingSet(7, trees, dm);
+        assertEquals(1, dominatingSet.size()); //single node
+    }
+
+    /**
+     * Test of trimDominatingSet method, of class
+     * DominatingSetCalculatorImplicit.
+     */
+    @Test
+    public void testTrimDominatingSet() {
+        DominatingSetCalculatorImplicit dsc = new DominatingSetCalculatorImplicit();
+        List<Integer> dominatingSet = dsc.getDominatingSet(0, trees, dm);
+        assertEquals(7, dominatingSet.size()); //all nodes
+
+        List<Integer> trimmedDominatingSet = dsc.trimDominatingSet(dominatingSet, trees, dm, 0);
+        //nothing changed
+        assertEquals(7, trimmedDominatingSet.size());
+
+        trimmedDominatingSet = dsc.trimDominatingSet(dominatingSet, trees, dm, 1);
+        assertTrue(trimmedDominatingSet.size() <= 4);
+
+        trimmedDominatingSet = dsc.trimDominatingSet(dominatingSet, trees, dm, 2);
+        assertTrue(trimmedDominatingSet.size() <= 3);
+
+        trimmedDominatingSet = dsc.trimDominatingSet(dominatingSet, trees, dm, 7);
+        assertEquals(1, trimmedDominatingSet.size()); //single node
+
+        //test it also works without a full base
+        trimmedDominatingSet = dsc.trimDominatingSet(dominatingSet, trees, dm, 2);
+        trimmedDominatingSet = dsc.trimDominatingSet(trimmedDominatingSet, trees, dm, 7);
+        assertEquals(1, trimmedDominatingSet.size()); //single node
+    }
+
+}

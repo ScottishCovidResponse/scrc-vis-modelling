35fcbca0cffa4d37c8dd6c3370ee758d66e7a43a
maxie12 <max.sondag@hotmail.com>
2022-02-01 11:35:31 +0000

    Delete Calendar.ts

--- ContactTracing/GraphVisualisation/RepresentativeTreesViz/ContactSimulationViz/src/Calendar.ts
+++
@@ -1,7 +0,0 @@
-// const picker = new Litepicker({
-//     element: document.getElementById('datepicker'),
-//     singleMode: false,
-//     tooltipNumber: (totalDays) => {
-//         return totalDays - 1;
-//     }
-// })

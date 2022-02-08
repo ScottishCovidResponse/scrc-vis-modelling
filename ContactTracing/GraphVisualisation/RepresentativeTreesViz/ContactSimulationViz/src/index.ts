a6df73cf886c5a9ac0c2d00363f95bbd44c02592
maxie12 <max.sondag@hotmail.com>
2022-02-08 16:14:28 +0000

    Remove old code

--- ContactTracing/GraphVisualisation/RepresentativeTreesViz/ContactSimulationViz/src/index.ts
+++ ContactTracing/GraphVisualisation/RepresentativeTreesViz/ContactSimulationViz/src/index.ts
@@ -6,7 +6,6 @@ import { initTreeGrid } from "./representativeGraph";
 import { updateAll } from "./updateFunctions";
 import { noneColorScheme } from './ColorSchemes';
 import { vars } from "./vizVariables";
-import { generateDensityPlot } from "./DensityPlot.ts";
 
 
 const repTreesDataInputLocation = "../data/TTPData08-02Update/RepTrees.json";
@@ -21,13 +20,8 @@ const gridNamesInputLocation = "../data/TTPData08-02Update/WalesGridmapCoordinat
 //Policies
 export let policyDataPresent = false; //Whether policy data is present
 
-//End policies
 export let repTreesData, allTreesData, metaData, gridNames: string[][];
 
-console.log("Testing DensityPlot");
-
-// const testDiv = d3.select("#treeGridDiv");
-// generateDensityPlot(testDiv);
 
 //load in all the data
 d3.json(repTreesDataInputLocation).then(function (repTreesDataInput) {
@@ -46,9 +40,7 @@ d3.json(repTreesDataInputLocation).then(function (repTreesDataInput) {
                 for (let i = 0; i < repTreesData.length; i++) {
                     const repTree = repTreesData[i];
                     if (repTree.maxEditDistance > 0) {
-                        // if (repTree.children.length != 0) { //Speedup option
                         repTreesDataFiltered.push(repTree);
-                        // }
                     }
                 }
                 repTreesData = repTreesDataFiltered;
